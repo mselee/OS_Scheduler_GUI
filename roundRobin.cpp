da87216adbbb2dd950b28e3e0ca6f6748c15261d
Mohamed Seleem <seleem1337@gmail.com>
2016-03-21 18:36:36 +0200

    Rename scheduler to Scheduler

--- roundRobin.cpp
+++ roundRobin.cpp
@@ -2,58 +2,58 @@
 #include "scheduler.h"
 void Scheduler::roundRobin ()
 {
-		list<Process>::iterator pointer;
+        list<Process>::iterator pointer;
 
-	double x , temp=0 , total_waiting_time =0 ,avg_waiting_time ;
-	cout<< "Enter Quantum Time" << endl ;
-		cin >> x ; 
+    double x , temp=0 , total_waiting_time =0 ,avg_waiting_time ;
+    cout<< "Enter Quantum Time" << endl ;
+        cin >> x ;
 
-		double z=_queue.size();
-		double Time =0 ;
+        double z=_queue.size();
+        double Time =0 ;
 		
-		while (!_queue.empty())
-		{
+        while (!_queue.empty())
+        {
 			
 		
-		if(pointer == _queue.end)
-		{
-		pointer=_queue.begin();	
-		}
-		else
-		{
-			if(pointer->getArrivalTime() <=Time)
-			{
-			pointer->start(Time);
-			Time += x;
-			pointer ->stop(Time);
-			if(pointer->finished())
-				{
-					total_waiting_time+= pointer->getWaitingTime();
+        if(pointer == _queue.end)
+        {
+        pointer=_queue.begin();
+        }
+        else
+        {
+            if(pointer->getArrivalTime() <=Time)
+            {
+            pointer->start(Time);
+            Time += x;
+            pointer ->stop(Time);
+            if(pointer->finished())
+                {
+                    total_waiting_time+= pointer->getWaitingTime();
 
-					_queue.erase(pointer);
-				}
-			++pointer ;
+                    _queue.erase(pointer);
+                }
+            ++pointer ;
 
-			}
+            }
 			
-			else	
-			{
-			pointer = _queue.begin() ;
-			pointer->start(Time);
-		    Time += x;
-			pointer ->stop(Time);
-			if(pointer->finished())
-				{
-					_queue.erase(pointer);
-				}			
-			++pointer ;
-			}
+            else
+            {
+            pointer = _queue.begin() ;
+            pointer->start(Time);
+            Time += x;
+            pointer ->stop(Time);
+            if(pointer->finished())
+                {
+                    _queue.erase(pointer);
+                }
+            ++pointer ;
+            }
 
-		}
+        }
 
 
-		}
-		avg_waiting_time= total_waiting_time/z;
-		 cout<<"AVERAGE WAITING TIME IS ==> "<<avg_waiting_time << endl ;
+        }
+        avg_waiting_time= total_waiting_time/z;
+         cout<<"AVERAGE WAITING TIME IS ==> "<<avg_waiting_time << endl ;
 }
 

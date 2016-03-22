04917036c4f548db09b15eceb954b027ea86d452
Mo'men Adel <mouemened@gmail.com>
2016-03-22 21:10:39 +0200

    Log Processes running on processor

--- Scheduler.h
+++ Scheduler.h
@@ -3,17 +3,39 @@
 
 #include <list>
 #include "Process.h"
+#include "log.h"
 #include <queue>
+#include <map>
 
 class Scheduler
 {
 
 private:
     size_t current_time;
+    size_t logStartTime;
     list<Process*> _queue;
-//    queue<Log> log;
+    queue<Log*> log;
+    map<size_t,string> colours;
+
+
     void sortSJF();
     void sortPriority();
+
+    void start(Process* p) {
+        p->start(current_time);
+
+        if(colours[p->getPID()] == "") {
+            colours[p->getPID()] = "#" + to_string(rand()%1000000);
+        }
+
+        logStartTime = current_time;
+    }
+
+    void stop(Process* p) {
+        p->stop(current_time);
+        size_t pid = p->getPID();
+        log.push(new Log(pid,logStartTime, current_time, colours[pid]));
+    }
 
 public:
 	Scheduler(list<Process*> queue) {

5d796c794911cf033720a7352e6fd6c4efa10fe3
Mo'men Adel <mouemened@gmail.com>
2016-03-22 19:19:17 +0200

    Final FCFS and roundrobin

--- fcfs.cpp
+++ fcfs.cpp
@@ -1,23 +1,29 @@
-#include "Scheduler.h"
+#include "scheduler.h"
 #include <iostream>
+using namespace std ;
 
 
 void Scheduler::fcfs ()
 {
-    int Time = 0;
+    list<Process*>::iterator pointer;
+    pointer =_queue.begin();
+
+    double Time = 0;
     double total_waiting_time =0 ,avg_waiting_time , temp=0 ;
-    list<Process>::iterator pointer;
-	
+
+
+
+
     while(pointer != _queue.end())
     {
-        pointer->start(Time);
-        Time = Time + pointer->getBurstTime();
-        pointer -> stop(Time);
-        total_waiting_time+=pointer->getWaitingTime();
+
+        (*pointer)->start(Time);
+        Time = Time + (*pointer)->getBurstTime();
+        (*pointer) -> stop(Time);
+        total_waiting_time+=(*pointer)->getWaitingTime();
         ++pointer;
     }
     avg_waiting_time= total_waiting_time/_queue.size();
          cout<<"AVERAGE WAITING TIME IS ==> "<<avg_waiting_time << endl ;
 
 }
-	
--- roundRobin.cpp
+++ roundRobin.cpp
@@ -1,59 +1,69 @@
 #include  <iostream>
 #include "scheduler.h"
+using namespace std;
 void Scheduler::roundRobin ()
 {
-        list<Process>::iterator pointer;
-
-    double x , temp=0 , total_waiting_time =0 ,avg_waiting_time ;
+        list<Process*>::iterator pointer;
+        pointer =_queue.begin();
+    double x;
+    double total_waiting_time =0;
+    double avg_waiting_time;
     cout<< "Enter Quantum Time" << endl ;
         cin >> x ;
-
         double z=_queue.size();
         double Time =0 ;
-		
+
+
+
         while (!_queue.empty())
         {
-			
-		
-        if(pointer == _queue.end)
+        if(pointer == _queue.end())
         {
         pointer=_queue.begin();
         }
         else
         {
-            if(pointer->getArrivalTime() <=Time)
+            if((*pointer)->getArrivalTime() <=Time)
             {
-            pointer->start(Time);
-            Time += x;
-            pointer ->stop(Time);
-            if(pointer->finished())
+                (*pointer)->start(Time);
+                if((*pointer)->getRemainingTime(Time) < x)
+                    Time += (*pointer)->getRemainingTime(Time);
+                else
+                    Time += x;
+                (*pointer) ->stop(Time);
+            if((*pointer)->isFinished())
                 {
-                    total_waiting_time+= pointer->getWaitingTime();
-
-                    _queue.erase(pointer);
+                    total_waiting_time+= (*pointer)->getWaitingTime();
+                    if(_queue.size() == 1) {
+                        _queue.erase(pointer);
+                        break;
+                    }
+                    else {
+                        --pointer;
+                        _queue.erase(next(pointer,1));
+                    }
                 }
             ++pointer ;
 
             }
-			
+
             else
             {
             pointer = _queue.begin() ;
-            pointer->start(Time);
-            Time += x;
-            pointer ->stop(Time);
-            if(pointer->finished())
-                {
-                    _queue.erase(pointer);
-                }
-            ++pointer ;
+            //(*pointer)->start(Time);
+            //Time += x;
+            //(*pointer) ->stop(Time);
+            //if((*pointer)-> isFinished())
+                //{
+                    //_queue.erase(pointer);
+                //}
+            //++pointer ;
             }
-
         }
-
-
-        }
+    }
+        if (_queue.size()==0){
         avg_waiting_time= total_waiting_time/z;
          cout<<"AVERAGE WAITING TIME IS ==> "<<avg_waiting_time << endl ;
+        }
 }
 

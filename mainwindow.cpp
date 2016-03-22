b630a2e452257f429d0eb94e7a7e50f9af770c24
Mohamed Seleem <seleem1337@gmail.com>
2016-03-22 21:05:01 +0200

    Wire the UI logic to the algorithms

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,9 +7,80 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    vLayout = new QVBoxLayout();
+    ui->scrollAreaWidgetContents->setLayout(vLayout);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btnAddRow_clicked()
+{
+
+    QWidget *horizontalWidget = new QWidget();
+    QHBoxLayout *hLayout = new QHBoxLayout(horizontalWidget);
+
+    QLineEdit* le1 = new QLineEdit(horizontalWidget);
+    QLineEdit* le2 = new QLineEdit(horizontalWidget);
+    QLineEdit* le3 = new QLineEdit(horizontalWidget);
+
+    le1->setObjectName("arrival-" + QString::number(index));
+    le2->setObjectName("burst-" + QString::number(index));
+    le3->setObjectName("priority-" + QString::number(index));
+    le3->setText("0");
+
+    hLayout->addWidget(le1);
+    hLayout->addWidget(le2);
+    hLayout->addWidget(le3);
+
+    vLayout->addWidget(horizontalWidget);
+    index++;
+}
+
+void MainWindow::on_comboAlgorithm_currentIndexChanged(int index)
+{
+    if (index == 1 || index == 2)
+        ui->comboPremp->setEnabled(true);
+    else
+        ui->comboPremp->setEnabled(false);
+    if (index == 3)
+        ui->gboxQuantum->setEnabled(true);
+    else
+        ui->gboxQuantum->setEnabled(false);
+}
+
+void MainWindow::on_btnStart_clicked()
+{
+    list<Process*> processes;
+    for (int i = 0; i < index; ++i) {
+
+        double arrival = ui->scrollAreaWidgetContents->findChild<QLineEdit*>("arrival-" + QString::number(i))->text().toDouble();
+        double burst = ui->scrollAreaWidgetContents->findChild<QLineEdit*>("burst-" + QString::number(i))->text().toDouble();
+        double priority = ui->scrollAreaWidgetContents->findChild<QLineEdit*>("priority-" + QString::number(i))->text().toDouble();
+
+        Process* p = new Process(i, arrival, burst, priority);
+        processes.push_back(p);
+    }
+
+    scheduler = new Scheduler(processes);
+
+    switch (ui->comboAlgorithm->currentIndex()) {
+    case 0:
+        scheduler->fcfs();
+        break;
+    case 1:
+        scheduler->sjf(ui->comboPremp->currentIndex());
+        break;
+    case 2:
+        scheduler->priority(ui->comboPremp->currentIndex());
+        break;
+    case 3:
+        scheduler->roundRobin(ui->txtQuantum->text().toDouble());
+        break;
+    default:
+        break;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QVBoxLayout>
+#include <QLineEdit>
+#include <QHBoxLayout>
 #include <QMainWindow>
+#include <list>
+#include "Scheduler.h"
+
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +22,18 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_btnAddRow_clicked();
+
+    void on_comboAlgorithm_currentIndexChanged(int index);
+
+    void on_btnStart_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QVBoxLayout* vLayout;
+    int index = 0;
+    Scheduler* scheduler;
 };
 
 #endif // MAINWINDOW_H

d2b9fdcec26f3130b0814b758db48168842e10ed
Mo'men Adel <mouemened@gmail.com>
2016-03-24 16:35:39 +0200

    Fix FCFS, use time of scheduler

--- fcfs.cpp
+++ fcfs.cpp
@@ -5,10 +5,9 @@ using namespace std ;
 
 void Scheduler::fcfs ()
 {
-    list<Process*>::iterator pointer;
+    list<Process*>::iterator pointer = _queue.begin();
     pointer =_queue.begin();
 
-    double Time = 0;
     double total_waiting_time =0 ,avg_waiting_time , temp=0 ;
 
 
@@ -16,12 +15,15 @@ void Scheduler::fcfs ()
 
     while(pointer != _queue.end())
     {
-
-        start((*pointer));
-        Time = Time + (*pointer)->getBurstTime();
-        stop((*pointer));
-        total_waiting_time+=(*pointer)->getWaitingTime();
-        ++pointer;
+        if((*pointer)->getArrivalTime() <= current_time) {
+            start((*pointer));
+            current_time = current_time + (*pointer)->getBurstTime();
+            stop((*pointer));
+            total_waiting_time+=(*pointer)->getWaitingTime();
+            ++pointer;
+        }
+        else
+            current_time++;
     }
     avg_waiting_time= total_waiting_time/_queue.size();
          cout<<"AVERAGE WAITING TIME IS ==> "<<avg_waiting_time << endl ;
